e0920663496c4da64c109d46c8ac016256a243f6
daria <daryashapovalova7@gmail.com>
2019-10-11 22:42:09 +0300

    change initial routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Route, Router, Switch, BrowserRouter} from "react-router-dom";
+import {Route, Switch, BrowserRouter, Redirect} from "react-router-dom";
 import Login from "../Login";
 import Registration from "../Registration";
 import UserProfile from "../UserProfile";
@@ -9,7 +9,11 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-       <Route exact path={'/login'} component={Login}/>
+        <Route exact path="/" render={() => (
+            <Redirect to="/login"/>
+        )}/>
+        {/*TODO: add check if user is logged in*/}
+        <Route exact path={'/login'} component={Login}/>
         <Route exact path={'/register'} component={Registration}/>
         <Route exact path={'/users/profile'} component={UserProfile}/>
       </Switch>
